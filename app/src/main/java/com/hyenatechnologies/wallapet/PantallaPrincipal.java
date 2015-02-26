2c3e46798410747333967291580e0c60f80da9fd
c0conut <raul.piraces@gmail.com>
2015-02-26 16:37:14 +0100

    Prueba

--- app/src/main/java/com/hyenatechnologies/wallapet/PantallaPrincipal.java
+++ app/src/main/java/com/hyenatechnologies/wallapet/PantallaPrincipal.java
@@ -11,7 +11,7 @@ import android.view.MenuItem;
 public class PantallaPrincipal extends ActionBarActivity {
 
     /**
-     *
+     * Metodo onCreate()
      * @param savedInstanceState
      */
     @Override
