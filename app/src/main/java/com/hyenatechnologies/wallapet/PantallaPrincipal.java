e6d1c375759e8d54603727a85e9f0a8d9b1ba674
teruyi <ssm.fuentes@gmail.com>
2015-03-28 17:40:13 +0100

    Merge branch 'master' of https://github.com/piraces/Wallapet
    Conflicts:
    	.idea/modules.xml
    	app/app.iml
    	app/src/main/java/com/hyenatechnologies/wallapet/PantallaPrincipal.java

--- app/src/main/java/com/hyenatechnologies/wallapet/PantallaPrincipal.java
+++ app/src/main/java/com/hyenatechnologies/wallapet/PantallaPrincipal.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-jihugyvf
+/**
+ * pruebas
+ */
 public class PantallaPrincipal extends ActionBarActivity {
 
     /**
