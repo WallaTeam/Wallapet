f8706fd6e3c07ec246c6313a6241de8dc4623a99
Ismael Rodríguez <ismaro.394@gmail.com>
2015-03-30 14:06:23 +0200

    Puesta en común de clases Anuncio y Cuenta con las del servidor.

--- app/src/main/java/com/hyenatechnologies/wallapet/Anuncio.java
+++ app/src/main/java/com/hyenatechnologies/wallapet/Anuncio.java
@@ -1,39 +1,98 @@
 package com.hyenatechnologies.wallapet;
 
-import java.util.ArrayList;
 import java.util.List;
-import android.widget.Button;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
 public class Anuncio {
+
+    private int idAnuncio;
+    private String email;
     private String estado;
+    private String descripcion;
+    private String tipoIntercambio;
+    private String especie;
+    private String rutaImagen;
+    private double precio;
+    private String titulo;
 
-    public String tipoIntercambio;
 
-    public String tipoAnimal;
-
-    public String descripcion;
-
-    public List<Animal> animal = new ArrayList<Animal> ();
-
-    public List<Order> order = new ArrayList<Order> ();
-
-    public List<Imagen> imagen = new ArrayList<Imagen> ();
-
-    public void cambiarEstado(final String nuevoEstado) {
+    public double getPrecio() {
+        return precio;
     }
 
-    public void modificar() {
+    public void setPrecio(double precio) {
+        this.precio = precio;
     }
 
-    public void getInfo() {
+    public String getTitulo() {
+        return titulo;
     }
 
-    public Order getOrder() {
-        // TODO Auto-generated return
-        return null;
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 
-    public void actualizar() {
+    //Pasa a JSON
+    public static String toJson(Anuncio a){
+        Gson gson = new Gson();
+
+        return gson.toJson(a);
     }
+
+    public static String listToJson(List<Anuncio> a){
+        Gson gson = new Gson();
+        return gson.toJson(a);
+    }
+
+    //Obtiene objeto de JSON
+    public static Anuncio fromJson(String json) throws JsonParseException{
+        Gson gson = new Gson();
+        return gson.fromJson(json, Anuncio.class);
+    }
+
+    public int getIdAnuncio() {
+        return idAnuncio;
+    }
+    public void setIdAnuncio(int idAnuncio) {
+        this.idAnuncio = idAnuncio;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public String getTipoIntercambio() {
+        return tipoIntercambio;
+    }
+    public void setTipoIntercambio(String tipoIntercambio) {
+        this.tipoIntercambio = tipoIntercambio;
+    }
+    public String getEspecie() {
+        return especie;
+    }
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+    public String getRutaImagen() {
+        return rutaImagen;
+    }
+    public void setRutaImagen(String rutaImagen) {
+        this.rutaImagen = rutaImagen;
+    }
+
 
 }
