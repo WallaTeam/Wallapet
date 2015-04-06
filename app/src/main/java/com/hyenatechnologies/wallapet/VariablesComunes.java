344bc17e3bac6e3886bf7fa2cb414e4087916f57
Raul Piraces Alastuey <raul.piraces@gmail.com>
2015-04-06 19:41:26 +0200

    Añadida nueva clase de prueba "VariablesComunes.java".

---
+++ app/src/main/java/com/hyenatechnologies/wallapet/VariablesComunes.java
@@ -0,0 +1,18 @@
+package com.hyenatechnologies.wallapet;
+
+
+public class VariablesComunes {
+
+    private int idAnuncio;
+    public VariablesComunes (){
+        idAnuncio = 0;
+    }
+
+    public void setAnuncioID (int i){
+        idAnuncio = i;
+    }
+
+    public int getAnuncioID (){
+        return idAnuncio;
+    }
+}
